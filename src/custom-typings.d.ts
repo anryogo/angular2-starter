3dfda412890dfdbead19f99ed162cc3195d5f173
Andrii Povelychenko <anryogo@gmail.com>
2017-04-15 20:10:36 +0300

    ability to import json files

--- src/custom-typings.d.ts
+++ src/custom-typings.d.ts
@@ -1,4 +1,12 @@
 /**
+ * Ability to import json files in typescript
+ */
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
+
+/**
  * Application environment variable
  */
 declare const APP_ENV: string;
